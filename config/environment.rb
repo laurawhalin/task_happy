8b2a365b8cf8cb558e42ede3afb12c4ebc8b68a5
Laura Whalin <laurawhalin@gmail.com>
2015-05-06 01:44:03 -0600

    removed broken mailer functionality

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :user_name            => ENV['gmail_username'],
+#   :password             => ENV['gmail_password'],
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
